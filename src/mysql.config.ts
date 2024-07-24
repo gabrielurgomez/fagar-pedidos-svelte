63a359328454e4ae84f7e46ab5f6796862817de1
Gabriel Uribe Gomez <info@colsysnet.com>
2024-07-23 20:53:58 -0500

    colocar credenciales en variables de entorno

--- src/mysql.config.ts
+++ src/mysql.config.ts
@@ -1,27 +1,12 @@
+import { env } from '$env/dynamic/private';
 import mysql, { type PoolOptions } from 'mysql2/promise';
 
 const access: PoolOptions = {
-    host: 'fagarcomercial.com',
-    user: 'fagarcomercial_user',
-    port: 3306,
-    database: 'fagarcomercial_admon',
-    password: 'VYg906Z8rApB',
+    host: env.DB_HOST,
+    user: env.DB_USER,
+    port: Number(env.DB_PORT),
+    database: env.DB_NAME,
+    password: env.DB_PASSWORD,
 };
 
 export const pool = mysql.createPool(access);
-
-
-//Conexion a la base de datos de admon, la que usan en produccion para crear productos e inventarios
-/*export const pool = mysql.createPool({
-    host: 'fagarcomercial',
-    user: 'fagarcomercial_user',
-    database: 'fagarcomercial_admon',
-    waitForConnections: true,
-    connectionLimit: 10,
-    maxIdle: 10,
-    idleTimeout: 60000,
-    queueLimit: 0,
-    enableKeepAlive: true,
-    keepAliveInitialDelay: 0,
-});*/
-
